438e7049ae166314cb29c8475315e99192856b9e
Baccarani Pham <baccarani_0103@live.ca>
2017-06-20 09:17:22 -0400

    homepage

--- src/app/dashboard/blank-page/blank-page.component.ts
+++ src/app/dashboard/blank-page/blank-page.component.ts
@@ -4,7 +4,8 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-blank-page',
-    templateUrl: './blank-page.component.html'
+    templateUrl: './blank-page.component.html',
+    styleUrls: ['./blank-page.component.css']
 })
 
 export class BlankPageComponent {
