0c1a66c99f36460a40f4a9741c8524fff101ee8d
Baccarani Pham <baccarani_0103@live.ca>
2017-06-21 22:23:18 -0400

    fixed formatting

--- src/app/dashboard/blank-page/blank-page.component.ts
+++ src/app/dashboard/blank-page/blank-page.component.ts
@@ -41,13 +41,13 @@ export class BlankPageComponent implements OnInit {
     totalPercent: string = '';
     */
 
-    privatePassenger: number = 0;
-    lightTrucks: number = 0;
-    mediumTrucks: number = 0;
-    heavyTrucks: number = 0;
-    extraHeavyTrucks: number = 0;
-    heavyTrucksTractors: number = 0;
-    extraHeavyTrucksTractors: number = 0;
+    privatePassenger: any = '' ;
+    lightTrucks: any = '' ;
+    mediumTrucks: any = '' ;
+    heavyTrucks: any = '' ;
+    extraHeavyTrucks: any = '' ;
+    heavyTrucksTractors: any = '' ;
+    extraHeavyTrucksTractors: any = '' ;
 
     limitAL: string = '';
     premiumAL: string = '';
